4d3a3a45c8cdfb5dbbf85af8a001a9ece86dfe6b
Benson Wong <mostlygeek+git@gmail.com>
2014-08-22 00:11:37 -0700

    change the channel example

--- 05-channels.go
+++ 05-channels.go
@@ -2,28 +2,41 @@ package main
 
 import (
     "fmt"
-    "time"
-    "math/rand"
 )
-
 
 func main() {
     // https://gobyexample.com/channels
 
-    // make a channel that can buffer 100 integers
-    myChannel := make(chan int, 100)
+    /* non buffered channels only fill if there is something
+       waiting to flush it
+     */
+    nonBuffChan  := make(chan int)
 
+    // need something to continue flushing it
     go func() {
         for {
-            m := <-myChannel
-            fmt.Println(m)
+            _ = <-nonBuffChan
         }
     }()
 
-    for {
-        myChannel <- rand.Intn(1000)
-        time.Sleep(250 * time.Millisecond)
+    nonBuffChan <- 1
+
+    /* buffered channels can take multiple values and
+       then be flushed later. If we go over "3" here
+       an error will be generated saying all goroutines are
+       asleep
+     */
+
+    bufferedChan := make(chan int, 3)
+    for i:=0; i<3; i++ {
+        bufferedChan <- i
     }
 
+    // here we can flush all the values in the buffered
+    // channel, but if we try for 4 we'll get the
+    // all goroutines are asleep error ...
+    for i:=0; i<3; i++ {
+        fmt.Printf("%d\n", <-bufferedChan)
+    }
 
 }
