c4c9894e8c9cc4e136dada6186edf5600591b6fe
Benson Wong <mostlygeek+git@gmail.com>
2014-12-28 23:22:41 -0800

    fix bugs in 07-packages

--- 07-packages/main.go
+++ 07-packages/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    /** Ref:
-     * https://golang.org/ref/spec#Import_declarations
-     */
+	/** Ref:
+	 * https://golang.org/ref/spec#Import_declarations
+	 */
 
-    /* will be package_example.Func */
-    "github.com/mostlygeek/go-quick/package_example"
+	/* will be package_example.Func */
+	"github.com/mostlygeek/go-quick/07-packages/package_example"
 
-    /* alias the package name */
-    an_alias "github.com/mostlygeek/go-quick/package_example"
+	/* alias the package name */
+	an_alias "github.com/mostlygeek/go-quick/07-packages/package_example"
 
-    // import into the local namespace so we can just
-    // use Add(...) directly
-    . "github.com/mostlygeek/go-quick/package_example"
+	// import into the local namespace so we can just
+	// use Add(...) directly
+	. "github.com/mostlygeek/go-quick/07-packages/package_example"
 )
 
 func main() {
-    fmt.Println(package_example.Add(1, 1))
+	fmt.Println(package_example.Add(1, 1))
 
-    // the alias
-    fmt.Println(an_alias.Add(1, 1))
+	// the alias
+	fmt.Println(an_alias.Add(1, 1))
 
-    // local name space imported fns
-    fmt.Println(Add(1, 1))
+	// local name space imported fns
+	fmt.Println(Add(1, 1))
 }
