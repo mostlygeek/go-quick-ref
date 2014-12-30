bdaff6309ef44d217ea9fcaf1627635dca83b4d8
Benson Wong <mostlygeek+git@gmail.com>
2014-12-30 15:23:20 -0800

    add more examples with interfaces for structs

--- 04-structs/interfaces.go
+++ 04-structs/interfaces.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Toggleable interface {
-	Enabled() bool
+	Enabled() (bool, string)
 }
 
 type Base struct {
@@ -20,7 +20,7 @@ func (b *Base) Name() string { return b.name }
 
 // implement the Toggleable interface on base
 // it will also work on Child!
-func (b Base) Enabled() bool { return b.enabled }
+func (b Base) Enabled() (bool, string) { return b.enabled, "base" }
 
 type Child struct {
 	Base
@@ -29,21 +29,32 @@ type Child struct {
 
 func (c Child) Age() int { return c.age }
 
+type Child2 struct{ Base }
+
+func (c Child2) Enabled() (bool, string) { return c.enabled, "child" }
+
 func IsEnabled(t Toggleable) string {
-	if t.Enabled() {
-		return "yes"
+	if enabled, msg := t.Enabled(); enabled == true {
+		return "yes, " + msg
+	} else {
+		return "no, " + msg
 	}
 
-	return "no"
 }
 
 func main() {
-	b := Base{false, "hello"}
+	b := Base{false, "Base"}
 	fmt.Printf("%v is enabled? %v\n", b.Name(), IsEnabled(b))
 
 	//c.Name() works because it includes Base as an
 	//anonymous field
 
 	c := Child{Base: Base{true, "Child"}, age: 10}
-	fmt.Printf("Child %v:%v is enabled? %v\n", c.Name(), c.Age(), IsEnabled(c))
+	fmt.Printf("%v:%v is enabled? %v\n", c.Name(), c.Age(), IsEnabled(c))
+
+	// Child2 has an Enabled() method so that is used instead of the
+	// one in Base like with Child1
+	c2 := Child2{Base: Base{true, "Child2"}}
+	fmt.Printf("Child2 %v is enabled? %v\n", c2.Name(), IsEnabled(c2))
+
 }
